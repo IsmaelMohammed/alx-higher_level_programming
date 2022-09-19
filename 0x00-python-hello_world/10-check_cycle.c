68ff8b9f78d4cee9b92f335d51029eed74a3080b
Ismael Mohammed <ismaelmohammed97@gmail.com>
2022-09-19 15:54:14 +0300

    Linked list cycle

--- 0x00-python-hello_world/10-check_cycle.c
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -1,23 +1,25 @@
 #include "lists.h"
+
 /**
- * check_cycle - check if loop exists in cycle
- * @list: struct to be checked
- *
- * Return: 1 if it is, 0 if not
- */
+*check_cycle - check if the singly list contain a cycle or not
+*@list: pointer to a singly list
+*
+*Return: 1 if successed, otherwise 0
+*/
 
 int check_cycle(listint_t *list)
 {
-	listint_t *turtle = list;
-	listint_t *hare = list;
+	listint_t *start, *new;
 
-	if (list == NULL)
+	if (list == NULL || list->next == NULL)
 		return (0);
-	while (turtle != NULL && hare != NULL && hare->next != NULL)
+
+	start = new = list;
+	while (start && new && new->next)
 	{
-		turtle = turtle->next;
-		hare = (hare->next)->next;
-		if (turtle == hare)
+		start = start->next;
+		new = new->next->next;
+		if (start == new)
 			return (1);
 	}
 	return (0);
