b2c35bbc96c87e468da687ba25f0569c39c29d44
Ismael Mohammed <ismaelmohammed97@gmail.com>
2022-09-20 14:01:18 +0300

    prints possible dt combnations of two digits

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
-for a in range(9):
-    for b in range(a, 10):
-        if a < b:
-            print("{:d}{:d}".format(a, b),
-                  end="\n" if a is 8 and b is 9 else ", ")
+
+
+for i in range(9):
+    for a in range(i + 1, 10):
+        if i != 8 or a != 9:
+            print("{:n}{:n}".format(i, a), end=", ")
+print("{:n}{:n}".format(i, a))
